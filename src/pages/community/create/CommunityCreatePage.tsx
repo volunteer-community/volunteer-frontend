e77d870fa77c54bea58972f9d381197b68dd8bf9
seon-mikim <tjsalsun@gmail.com>
2023-11-06 06:23:18 +0900

    feat: community  contanst 생성
    - 상수 파일 따로 생성으로 불필요한 코드 삭제

--- src/pages/community/create/CommunityCreatePage.tsx
+++ src/pages/community/create/CommunityCreatePage.tsx
@@ -1,18 +1,32 @@
+import { createCommunity } from '@apis/community/post';
 import CommunityForm from '@components/CommunityForm/CommunityForm';
 import Section from '@components/ui/Section/Section';
+import { AxiosResponse } from 'axios';
+import { useMutation, useQueryClient } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import { INITIDATA } from 'src/constants/community';
 
-const INITIDATA = {
-   title: '',
-    content: '',
-    categoryType: '',
-    maxParticipant: 10,
-    location: '',
-    file: [],
-}
+
 const CommunityCreatePage = () => {
+  const navigate = useNavigate()
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation<
+    AxiosResponse<any>,
+    unknown,
+    { communityData: FormData; categoryType: string }, 
+    unknown
+  >(
+    ({ communityData, categoryType }) => createCommunity({ formData:communityData, categoryType }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('community');
+        navigate('/')
+      },
+    }
+  );
   return (
-		<Section sectionTitle='커뮤니티 생성'>
-      <CommunityForm initialData={INITIDATA}/>
+    <Section sectionTitle="커뮤니티 생성">
+      <CommunityForm initialData={INITIDATA} onSave={mutate} />
     </Section>
   );
 };
