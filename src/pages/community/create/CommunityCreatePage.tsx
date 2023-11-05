a649b4fcaa86ae92ca52dcf211b7aee173249f3f
seon-mikim <tjsalsun@gmail.com>
2023-11-06 07:34:29 +0900

    refactor: 커스텀 훅생성으로 불필요 코드 삭제
    - 가독성을 위한 수정 및 리팩토링

--- src/pages/community/create/CommunityCreatePage.tsx
+++ src/pages/community/create/CommunityCreatePage.tsx
@@ -1,32 +1,14 @@
-import { createCommunity } from '@apis/community/post';
-import CommunityForm from '@components/CommunityForm/CommunityForm';
-import Section from '@components/ui/Section/Section';
-import { AxiosResponse } from 'axios';
-import { useMutation, useQueryClient } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 import { INITIDATA } from '@constants/community';
+import Section from '@components/ui/Section/Section';
+import CommunityForm from '@components/CommunityForm/CommunityForm';
+import { useCreateCommunity } from '@hooks/queries/community';
 
 
 const CommunityCreatePage = () => {
-  const navigate = useNavigate()
-  const queryClient = useQueryClient();
-  const { mutate } = useMutation<
-    AxiosResponse<any>,
-    unknown,
-    { communityData: FormData; categoryType: string }, 
-    unknown
-  >(
-    ({ communityData, categoryType }) => createCommunity({ formData:communityData, categoryType }),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries('community');
-        navigate('/')
-      },
-    }
-  );
+  const { handleCreateCommunity } = useCreateCommunity();
   return (
     <Section sectionTitle="커뮤니티 생성">
-      <CommunityForm initialData={INITIDATA} onSave={mutate} />
+      <CommunityForm initialData={INITIDATA} onSave={handleCreateCommunity} />
     </Section>
   );
 };
