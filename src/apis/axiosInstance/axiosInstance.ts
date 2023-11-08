3695b3805afafa72627b1d97a36a000554231cfe
seon-mikim <tjsalsun@gmail.com>
2023-11-09 05:33:22 +0900

    fix: 토큰 갱신처리 되지 않음 수정

--- src/apis/axiosInstance/axiosInstance.ts
+++ src/apis/axiosInstance/axiosInstance.ts
@@ -24,9 +24,8 @@ const createInstance = (contentType: string) => {
   instance.interceptors.response.use(
     (response) => response,
     async (error) => {
-      console.log(contentType);
-      const { status } = error.response;
       const refreshToken = getCookie('refreshToken');
+      const { status } = error.response;
       if (status === 401) {
         const response = await reissueToken(refreshToken);
         return response;
