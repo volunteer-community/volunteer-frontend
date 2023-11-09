22dbb496ede95e0a63f055e7e3117db849ab07b1
seon-mikim <tjsalsun@gmail.com>
2023-11-10 03:45:14 +0900

    feat: 커뮤니티 상세페이지 링크 컴포넌트 state 추가

--- src/pages/community/[communityId]/edit/CommunityEditPage.tsx
+++ src/pages/community/[communityId]/edit/CommunityEditPage.tsx
@@ -2,6 +2,7 @@ import CommunityForm from "@components/CommunityForm/CommunityForm"
 import Section from "@components/ui/Section/Section"
 import Logo from '@assets/images/Logo.png';
 import Camera from '@assets/images/camara_icon.svg';
+import { useLocation } from "react-router-dom";
 
 const INITIDATA = {
   communityTitle: '텀블러를 사랑하는 모임',
@@ -15,6 +16,9 @@ const INITIDATA = {
 const INIT_EDIT_IMAGE = [Logo, Camera]
 
 const CommunityEditPage = () => {
+  const location = useLocation()
+
+  console.log(location.state.data);
 	return (
     <Section sectionTitle="커뮤니티 수정">
 			<CommunityForm initialData={INITIDATA} initialImageURLs={INIT_EDIT_IMAGE }  />
