0e6b67c57bf839d816aaf48317b94f09d3cb4585
seon-mikim <tjsalsun@gmail.com>
2023-11-15 23:16:45 +0900

    feat: 휴대폰 중복확인 쿼리 훅 생성

--- src/hooks/queries/signup/index.ts
+++ src/hooks/queries/signup/index.ts
@@ -1,8 +1,8 @@
-import { createUserAddInfo } from '@apis/auth/signup';
+import { checkData, checkUserPhone, createUserAddInfo } from '@apis/auth/signup';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
-const useCreateUserAddInfo = () => {
+export const useCreateUserAddInfo = () => {
   const navigate = useNavigate();
   const { mutate } = useMutation((userInfo: Blob) => createUserAddInfo(userInfo), {
     onSuccess: () => {
@@ -11,4 +11,13 @@ const useCreateUserAddInfo = () => {
   });
   return { mutate };
 };
-export default useCreateUserAddInfo;
+
+
+export const useCheckUserPhone = () => {
+  const { mutate } = useMutation(checkUserPhone)
+  
+  const handleUserPhone = (phoneCheck:checkData) => {
+    mutate(phoneCheck)
+  }
+  return {handleUserPhone}
+}
